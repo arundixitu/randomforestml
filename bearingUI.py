fff163042bfe263d1fe3b4949cbb6a6c5eb6a446
arundixitu <arundixitu@vvce.ac.in>
2024-12-19 00:43:56 +0530

    Update bearingUI.py

--- bearingUI.py
+++ bearingUI.py
@@ -18,7 +18,7 @@ def visualize_class_distribution(data):
 def visualize_feature_importance():
     st.subheader("Feature Importance")
     feature_importance = rf_model.feature_importances_
-    feature_names = ['RMS', 'KU', 'CF', 'IF', 'PP', 'EN']
+    feature_names = ['PP', 'RMS', 'KU', 'IF']
     plt.figure(figsize=(8, 6))
     plt.barh(feature_names, feature_importance, color='teal')
     plt.xlabel("Importance")
